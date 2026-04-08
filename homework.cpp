86be8c069a199b85b68ed2aebd5d541c90bc2c6d
gofish8999 <gofish8999@gmail.com>
2026-04-08 17:12:09 +0900

    ddd

---
+++ homework.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <cctype>
+#include <list>
+
+
+
+
+
+
+
+bool did_all_hw(const Student_info& s)
+{
+    return ((find(s.homework.begin(), s.homework.end(), 0)) == s.homework.end());
+}
